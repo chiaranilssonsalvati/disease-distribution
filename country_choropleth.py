3d7c85bcc0f5b749790b301e4b1dd4a377cec246
Rebecca Muratore <rebeccamuratore@BridgeUp-3.local>
2016-08-17 11:27:47 -0400

    country choropleth

--- country_choropleth.py
+++ country_choropleth.py
@@ -1,20 +1,34 @@
 import plotly.plotly as ply
 import pandas as pd 
+import csv 
+
 
 ply.sign_in('rmm99', 'ko9rku5kqy')
 
-
-country_death_rates= pd.read_csv('country_death_rates.csv')
-
+'''
+country_death_rates= pd.read_csv("country_death_rates.csv")
+country_death_rates_2012= []
 
 for col in country_death_rates.columns:
 	country_death_rates[col]= country_death_rates[col].astype(str)
+	if country_death_rates
+'''
+country_death_rates_in = open("country_death_rates.csv", "rU")
+stats_in = list(csv.reader(country_death_rates_in))
+country_death_rates_2012={'Country':[],'All Causes':[],'Communicable & other Group I':[],'Noncommunicable diseases':[],'Injuries':[],'text':[]}
+for i in range(1,len(stats_in),2):
+	country_death_rates_2012['Country'].append(stats_in[i][0])
+	country_death_rates_2012['All Causes'].append(float(stats_in[i][2]))
+	country_death_rates_2012['Communicable & other Group I'].append(stats_in[i][3])
+	country_death_rates_2012['Noncommunicable diseases'].append(stats_in[i][4])
+	country_death_rates_2012['Injuries'].append(stats_in[i][5])
+	country_death_rates_2012['text'].append(stats_in[i][0] + "<br> Communicable & other Group I Diseases: " + stats_in[i][3] + "<br> Noncommunicable Diseases: " + stats_in[i][4] + "<br> Injuries: " + stats_in[i][5])
 
 geo_deets = dict(
 		scope = 'world',
 		projection = dict(type='Mercator'),
-		showlakes = True,
-		lakecolor = 'turquoise'
+		showlakes = False
+		
 	)
 
 
@@ -25,22 +39,24 @@ map_layout = dict(
 
 blue_scale = [[0.0, 'white'], [1.0, 'blue']]
 
-country_death_rates['text'] = country_death_rates['Country'] + "<br> Communicable & other Group I Diseases: " + country_death_rates['Communicable & other Group I'] + "<br> Noncommunicable Diseases: " + country_death_rates['Noncommunicable diseases'] + "<br> Injuries: " + country_death_rates['Injuries']
+#country_death_rates_2012['text'] = country_death_rates_2012['Country'] + "<br> Communicable & other Group I Diseases: " + country_death_rates_2012['Communicable & other Group I'] + "<br> Noncommunicable Diseases: " + country_death_rates_2012['Noncommunicable diseases'] + "<br> Injuries: " + country_death_rates_2012['Injuries']
 
+'''
 for value in country_death_rates['Year']:
 	if value == 2012:
 		country_death_rates['text'] 
 	#else:
 		#print('error')
+'''
 
 stuff_plotly_needs = [ dict(
 		type='choropleth',
 		colorscale= blue_scale,
 		autocolorscale= False,
-		locations = country_death_rates['Country'],
-		z = country_death_rates['All Causes'].astype(float),
+		locations = country_death_rates_2012['Country'],
+		z = country_death_rates_2012['All Causes'],
 		locationmode= 'country names',
-		text= country_death_rates['text'],
+		text= country_death_rates_2012['text'],
 		marker = dict(
 			line = dict(
 				color='black',
